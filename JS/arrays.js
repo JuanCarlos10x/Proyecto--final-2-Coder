db8442d3cf7187699328abed617cb1f96cc4b182
Carlos Sanchez <sanchezortizjuancarlos@gmail.com>
2022-09-04 01:10:24 -0500

    ejercicio con arrays

---
+++ JS/arrays.js
@@ -0,0 +1,11 @@
+// Se pide la edad, para saber si puedes pasar o no
+
+// Declaracion de mi variable
+let edad = [];
+edad = prompt("Ingresa tu edad, para saber si puedes pasar"); // Hago que cambie el valor de mi variable, para que pueda hacer la pregunta
+
+if (edad >= 18){
+    alert("Puedes entrar, adelnate") // Si tiene 18 o más años, si puede pasar
+} else {
+    alert("No puedes entrar eres un niño, corre ve te habla tu mamá"); // Si no tiene la mayoria de edad, no puede pasar
+}
