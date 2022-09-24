89bc87fd712a6a2b9d0eb3c27d9101dc2c91c8a7
Carlos Sanchez <sanchezortizjuancarlos@gmail.com>
2022-09-23 23:56:19 -0500

    operadores avanzados

---
+++ JS/operadores.js
@@ -0,0 +1,132 @@
+// variables
+
+const carrito = document.querySelector('#carrito');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
+const listaCursos = document.querySelector('#lista-cursos');
+let artiuclosCarrito = [];
+
+// event listeners
+cargarEventListeners();
+
+function cargarEventListeners() {
+    // cuando agregas un curso presionado 'agregar al carrito'
+    listaCursos.addEventListener('click', agregarCurso);
+
+    // elimina curso del carrito
+    carrito.addEventListener('click', eliminarCurso);
+
+    // muestra los cursos del carrito
+    document.addEventListener('DOMContentLoaded', () => {
+        artiuclosCarrito = JSON.parse(localStorage.getItem('carrito')) || [];
+
+        carritoHTML();
+    })
+
+    // vaciar el carrito
+    vaciarCarritoBtn.addEventListener('click', () => {
+        artiuclosCarrito = []; // reseteamos el carrito
+
+        limpiarHTML(); // elimina todo el HTML
+    })
+}
+
+// funciones
+
+// agrega un curso al carrito
+function agregarCurso(e) {
+    e.preventDefault();
+
+    if (e.target.classList.contains('agregar-carrito')) {
+        const cursoSeleccionado = e.target.parentElement.parentElement;
+        leerDatosCurso(cursoSeleccionado);
+    }
+}
+
+// elimina un curso del carrito
+function eliminarCurso(e) {
+    if (e.target.classList.contains('borrar-curso')) {
+        const cursoId = e.target.getAttribute('data-id');
+
+        // elimina del arreglo del articulosCarrito por el data-id
+        artiuclosCarrito = artiuclosCarrito.filter(curso => curso.id !== cursoId);
+
+        carritoHTML(); // iterar sobre el carrito y mostrar su HTML
+    }
+}
+
+// lee el contenido del HTML al que le dimos click y extrae la informacion del curso
+function leerDatosCurso(curso) {
+    // crear un objeto con el contenido del curso actual
+    const infoCurso = {
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio').textContent,
+        id: curso.querySelector('a').getAttribute('data-id'),
+        cantidad: 1
+    }
+
+    // revisa si un elemto ya existe en el carrtio
+    const existe = artiuclosCarrito.some(curso => curso.id === infoCurso.id);
+    if (existe) {
+        // actualizar la cantidad
+        const cursos = artiuclosCarrito.map(curso => {
+            if (curso.id === infoCurso.id) {
+                curso.cantidad++
+                return curso; // retorna el objeto actualizado
+            } else {
+                return curso; // retorna los objetos que no son los duplicados
+            }
+        });
+        artiuclosCarrito = [...cursos];
+    } else {
+        // agregamos el curso al carrito
+        artiuclosCarrito = [...artiuclosCarrito, infoCurso];
+    }
+
+    console.log(artiuclosCarrito)
+
+    carritoHTML();
+}
+
+// muestra el carrito de compras en el HTML
+function carritoHTML() {
+
+    // limpiar el html
+    limpiarHTML();
+
+    // recorre el carrito y genera el HTML
+    artiuclosCarrito.forEach(curso => {
+        const { imagen, titulo, precio, cantidad, id } = curso;
+        const row = document.createElement('tr')
+        row.innerHTML = `
+            <td>
+                <img src="${imagen}" width="100">
+            </td>
+                <td>${titulo}</td>
+                <td>${precio}</td>
+                <td>${cantidad}</td>
+            <td>
+                <a href="#" class="borrar-curso" data-id="${id}">X</a>
+            </td>
+        `;
+
+        // agregando el html del carrito en el tbody
+        contenedorCarrito.appendChild(row)
+    });
+
+    // agregar al carrito de compras al storage
+    sincronizarStorage();
+
+}
+
+function sincronizarStorage() {
+    localStorage.setItem('carrito', JSON.stringify(artiuclosCarrito));
+}
+
+// eliminar los elemtos del tbody
+function limpiarHTML() {
+    while (contenedorCarrito.firstChild) {
+        contenedorCarrito.removeChild(contenedorCarrito.firstChild);
+    }
+}
